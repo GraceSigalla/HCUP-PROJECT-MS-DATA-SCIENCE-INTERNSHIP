51891288c09bb0a8556d49ced1f1b1403e38a64e
GraceSigalla <109298506+GraceSigalla@users.noreply.github.com>
2022-08-09 15:24:46 -0400

    Update Work in Progress.R

--- Work in Progress.R
+++ Work in Progress.R
@@ -63,11 +63,6 @@ age_vars <- c(
   female_85_over = "B01001_049"
 )
 
-male_under_5 <- "B01001_003E"
-female_under_5 <-"B01001_027E"
-
-age_under5 <-  sum("male_under_5","female_under_5")
-age_under5
 
 ##Setting up race variables
 
@@ -81,150 +76,144 @@ race_vars <- c(
 )
 
 
+##Setting up household burden variables, for percentage of population spending 30%
+##or more of income on rent and those who own a house
+housing_burden_vars <- c(
+  owner_less_20k = "B25106_006",
+  owner_less_35k = "B25106_010",
+  owner_less_50k = "B25106_014", 
+  owner_less_75k = "B25106_018",
+  owner_over_75k = "B25106_022",
+  renter_less_20k = "B25106_028",
+  renter_less_35k = "B25106_032",
+  renter_less_50k = "B25106_036", 
+  renter_less_75k = "B25106_040",
+  renter_over_75k = "B25106_044"
+)
 
-test1 <- get_acs(
+
+ny_info_data <- get_acs(
   geography = "tract",
   state = 36,
-  variables =race_vars,
+  variables =c(
+    male_under_5 = "B01001_003", 
+    male_5_9 = "B01001_004", 
+    male_10_14 ="B01001_005",  
+    male_15_17 = "B01001_006", 
+    male_18_19 = "B01001_007",
+    male_20 = "B01001_008",
+    male_21 = "B01001_009",
+    male_22_24 ="B01001_010",
+    male_25_29 = "B01001_011",
+    male_30_34 = "B01001_012",
+    male_35_39 = "B01001_013",
+    male_40_44 = "B01001_014",
+    male_45_49 = "B01001_015",
+    male_50_54 = "B01001_016",
+    male_55_59 = "B01001_017",
+    male_60_61 = "B01001_018",
+    male_62_64 = "B01001_019",
+    male_65_66 = "B01001_020",
+    male_67_69 = "B01001_021",
+    male_70_74 = "B01001_022",
+    male_75_79 = "B01001_023",
+    male_80_84 = "B01001_024",
+    male_85_over = "B01001_025",
+    female_under_5 = "B01001_027",
+    female_5_9 = "B01001_028", 
+    female_10_14 = "B01001_029",
+    female_15_17 = "B01001_030", 
+    female_18_19 =  "B01001_031",
+    female_20 = "B01001_032",
+    female_21 = "B01001_033", 
+    female_22_24 = "B01001_034",
+    female_25_29 = "B01001_035",
+    female_30_34 = "B01001_036",
+    female_35_39 = "B01001_037",
+    female_40_44 = "B01001_038",
+    female_45_49 = "B01001_039",
+    female_50_54 = "B01001_040",
+    female_55_59 = "B01001_041",
+    female_60_61 = "B01001_042",
+    female_62_64 = "B01001_043",
+    female_65_66 = "B01001_044",
+    female_67_69 = "B01001_045",
+    female_70_74 = "B01001_046",
+    female_75_79 = "B01001_047",
+    female_80_84 = "B01001_048",
+    female_85_over = "B01001_049",
+    white = "B03002_003",
+    black = "B03002_004",
+    native = "B03002_005",
+    asian = "B03002_006",
+    HIPI = "B03002_007",
+    hispanic = "B03002_012",
+    owner_less_20k = "B25106_006",
+    owner_less_35k = "B25106_010",
+    owner_less_50k = "B25106_014", 
+    owner_less_75k = "B25106_018",
+    owner_over_75k = "B25106_022",
+    renter_less_20k = "B25106_028",
+    renter_less_35k = "B25106_032",
+    renter_less_50k = "B25106_036", 
+    renter_less_75k = "B25106_040",
+    renter_over_75k = "B25106_044"),
   year = 2019,
   output = "wide"
 )
-test1
-view(test1)
+view(ny_info_data)
 
-##Setting up household burden variables, for percentage of population spending 30%
-##or more of income on rent or those who own a house
-housing_burden_vars <- c(
-  owner_less_20k <- "B25106_006",
-  owner_less_35k <- "B25106_010",
-  owner_less_50k <- "B25106_014", 
-  owner_less_75k <- "B25106_018",
-  owner_over_75k <- "B25106_022",
-  renter_less_20k <- "B25106_028",
-  renter_less_35k <- "B25106_032",
-  renter_less_50k <- "B25106_036", 
-  renter_less_75k <- "B25106_040",
-  renter_over_75k <- "B25106_044"
-)
-#is.na(housing_burden_vars)
 
-owner_less_20k <- "B25106_006" 
-owner_less_35k <- "B25106_010"
-owner_less_50k <- "B25106_014" 
-owner_less_75k <- "B25106_018"
-owner_over_75k <- "B25106_022"
-renter_less_20k <- "B25106_028"
-renter_less_35k <- "B25106_032"
-renter_less_50k <- "B25106_036" 
-renter_less_75k <- "B25106_040"
-renter_over_75k <- "B25106_044"
+#Merging the variables to make the table smaller and more readable
+ny_info_data2 <- ny_info_data%>%
+  mutate(age_under5 = male_under_5 + female_under_5,
+         age_5_9 = male_5_9 + female_5_9,
+         age_10_14 = male_10_14 + female_10_14,
+         age_15_19 = male_15_17 + male_18_19 + female_15_17 + female_18_19,
+         age_20_24 = male_20 + male_21 + male_22_24 + female_20 + female_21 + female_22_24,
+         age_25_29 = male_25_29 + female_25_29,
+         age_30_34 = male_30_34 + female_30_34,
+         age_35_39 = male_35_39 + female_35_39,
+         age_40_44 = male_40_44 + female_40_44,
+         age_45_49 = male_45_49 + female_45_49,
+         age_50_54 = male_50_54 + female_50_54,
+         age_55_59 = male_55_59 + female_55_59,
+         age_60_64 = male_60_61 + male_62_64 + female_60_61 + female_62_64,
+         age_65_69 = male_65_66 + male_67_69 + female_65_66 + female_67_69,
+         age_70_74 = male_70_74 + female_70_74,
+         age_75_79 =  male_75_79 + female_75_79,
+         age_80_84 =  male_80_84 + female_80_84,
+         age_85_over = male_85_over + female_85_over,
+         owner_housing_burden = owner_less_20k + owner_less_35k + owner_less_50k +owner_less_75k + owner_over_75k,
+         renter_housing_burden = renter_less_20k + renter_less_35k + renter_less_50k + renter_less_75k + renter_over_75k)
+
+
+
+
+
+
+
+
+
 
 # Sum of owners and renters for housing burden
 
 practice1$owner_housing_burden = practice1$B25106_006E + practice1$B25106_010E + practice1$B25106_014E + practice1$B25106_018E + practice1$B25106_022E
 practice1$renter_housing_burden <-  sum(as.numeric("renter_less_20k"), as.numeric("renter_less_35k"), as.numeric("renter_less_50k"), as.numeric("renter_less_75k"),as.numeric("renter_less_75k"))
 # Practice
-practice1 <- get_acs(
-  geography = "tract",
-  state = 36,
-  variables =housing_burden_vars,
-  year = 2019,
-  output = "wide"
-)
-practice1
-view(practice1)
 
-#Pulling all race, age and housing burden separately
+
 
 ##Pulling housing burden data
-ny_housing_burd <- get_acs(
-  geography = "county",
-  state = 36,
-  variables = housing_burden_vars,
-  year = 2019,
-  output = "wide"
-)
-ny_housing_burd
-view(ny_housing_burd)
-
-
-#Pulling agegroup, race, sex and ethnicity
-ny_est <- get_estimates(
-  geography = "county",
-  product = "characteristics",
-  breakdown = c("AGEGROUP", "RACE" ,"SEX","HISP"),
-  breakdown_labels = TRUE,
-  state = 36,
-  year = 2019,
-  output = "wide"
-)
-View(ny_est)
-
-
-joined <- merge(ny_housing_burd, ny_est, by = "GEOID", all = TRUE)
-view(joined)
-
-
-#PULLING DATA USING ZCTA 
-
-
-#Pulling race data
-race_info <- get_acs(
-  geography = "zcta",
-  state = 36,
-  variables = race_vars,
-  year = 2019,
-  total = total_population,
-  output = "wide"
-)
-race_info
-view(race_info)
-
-##Pulling housing burden data
-burden <- get_acs(
-  geography = "zcta",
-  state = 36,
-  variables = housing_burden_vars,
-  year = 2019,
-  output = "wide"
-)
-burden
-view(burden)
-
-#Pulling median age, race & housing burden
-ny_info <- get_acs(
-  geography = "zcta",
-  state = 36,
-  variables = c(housing_burden_vars,race_vars, median_age = "B01002_001"),
-  year = 2019,
-  output = "wide"
-)
-ny_info
-view(ny_info)
-
-
-my_info1<-my_info%>%
-#IMPORTING EXCEL FILE TO R - ZIPCODE CROSSWALK
-
-zcta_df <- data.table(pipe("pbpaste"), sep = "\t", header = TRUE)
-zcta_df
-view(zcta_df)
-
-#EXERCISE*data.table - easier to save file as csv. or fread...TRY and get the same answer using the crosswalks, left join on GEOID (allocation factor), groub by ZCTA. 
-
-
-#Merge ny_info with zcta_df
-
-zip_df <- merge(ny_info, zcta_df, by.x ="GEOID", by.y= "ZCTA", all = TRUE)
-view(both_df)
-
-
-
-
-
-
-
-
+#ny_housing_burd <- get_acs(
+ # geography = "county",
+ # state = 36,
+  #variables = housing_burden_vars,
+  #year = 2019,
+  #output = "wide"
+#)
+#ny_housing_burd
+#view(ny_housing_burd)
 
 
 #Pulling agegroup, race, sex and ethnicity
@@ -239,12 +228,70 @@ view(both_df)
 #)
 #iew(ny_est)
 
+#PULLING DATA USING ZCTA 
 
 
+#Pulling race data
+#race_info <- get_acs(
+ # geography = "zcta",
+  #state = 36,
+  #variables = race_vars,
+  #year = 2019,
+  #total = total_population,
+  #output = "wide"
+#)
+#race_info
+#view(race_info)
 
-zip_df <- merge(ny_info, zct by = "GEOID", all = TRUE)
-view(both_df)
+##Pulling housing burden data
+#burden <- get_acs(
+#  geography = "zcta",
+ # state = 36,
+  #variables = housing_burden_vars,
+  #year = 2019,
+  #output = "wide"
+#)
+#burden
+#iew(burden)
 
+#Pulling median age, race & housing burden
+#ny_info <- get_acs(
+ # geography = "zcta",
+ # state = 36,
+ # variables = c(housing_burden_vars,race_vars, median_age = "B01002_001"),
+  #year = 2019,
+ # output = "wide"
+#)
+#ny_info
+#view(ny_info)
+
+
+#my_info1<-my_info%>%
+
+
+#IMPORTING EXCEL FILE TO R - ZIPCODE CROSSWALK
+
+#zcta_df <- data.table(pipe("pbpaste"), sep = "\t", header = TRUE)
+#zcta_df
+#view(zcta_df)
+
+#EXERCISE*data.table - easier to save file as csv. or fread...TRY and get the same answer using the crosswalks, left join on GEOID (allocation factor), groub by ZCTA. 
+
+
+#Pulling agegroup, race, sex and ethnicity
+#ny_est <- get_estimates(
+ # geography = "county",
+  #product = "characteristics",
+  #breakdown = c("AGEGROUP", "RACE" ,"SEX","HISP"),
+  #breakdown_labels = TRUE,
+  #state = 36,
+  #year = 2019,
+  #output = "wide"
+#)
+#iew(ny_est)
+
+#zip_df <- merge(ny_info, zct by = "GEOID", all = TRUE)
+#view(both_df)
 
 ##Setting up age variables (can't use this, brings out an error)
 #age_vars <- c(
